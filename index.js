0e7de1128e48b36796e498c0b3bdfd623645fce6
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2023-12-14 22:48:50 +0530

    adding the utils

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//It's a middleware that actually maps where are our static assets
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(3000, () => {
     console.log("Server started");
