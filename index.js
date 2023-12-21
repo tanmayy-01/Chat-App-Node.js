08cfb1b80d8ca5540fff62bb765fca5e6c74e282
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2023-12-21 13:17:21 +0530

    added the middleware for the static files

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-//It's a middleware that actually maps where are our static assets
+//It's a middleware that actually maps, where are our static assets
 app.use('/', express.static(__dirname + '/public'));
 
 server.listen(1050, () => {
